5b5d9ea892f634b7416b3edb0f3ad28d049fdfe5
ducpt-xusux <ducpt.xusux@gmail.com>
2026-08-18 09:32:54 +0700

    fix: Adding table seeder to DatabaseSeeder.php

---
+++ tests/Unit/DatabaseSeederTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Tests\Unit;
+
+use Database\Seeders\AddressSearchSeeder;
+use Database\Seeders\ColorSeeder;
+use Database\Seeders\DatabaseSeeder;
+use Database\Seeders\DesignerDataSeeder;
+use Database\Seeders\IndustrySeeder;
+use Database\Seeders\KeywordLanguageSeeder;
+use Database\Seeders\KeywordSeeder;
+use Database\Seeders\LogoLanguageSeeder;
+use Database\Seeders\LogoSeeder;
+use Database\Seeders\LogoZipSeeder;
+use Database\Seeders\LogSeeder;
+use Database\Seeders\OrderLogoSeeder;
+use Database\Seeders\OrderSeeder;
+use Database\Seeders\PackageProductCodeSeeder;
+use Database\Seeders\PackageSeeder;
+use Database\Seeders\SettingSeeder;
+use Database\Seeders\TasteSeeder;
+use Database\Seeders\UserSeeder;
+use Tests\TestCase;
+
+class DatabaseSeederTest extends TestCase
+{
+    public function test_it_runs_every_application_seeder_in_dependency_order(): void
+    {
+        $databaseSeeder = new class extends DatabaseSeeder
+        {
+            /** @var array<int, class-string> */
+            public array $seeders = [];
+
+            public function call($class, $silent = false, array $parameters = [])
+            {
+                $this->seeders = is_array($class) ? $class : [$class];
+
+                return $this;
+            }
+        };
+
+        $databaseSeeder->run();
+
+        $this->assertSame([
+            UserSeeder::class,
+            ColorSeeder::class,
+            IndustrySeeder::class,
+            TasteSeeder::class,
+            KeywordSeeder::class,
+            KeywordLanguageSeeder::class,
+            AddressSearchSeeder::class,
+            DesignerDataSeeder::class,
+            LogoSeeder::class,
+            LogoLanguageSeeder::class,
+            LogoZipSeeder::class,
+            PackageSeeder::class,
+            PackageProductCodeSeeder::class,
+            OrderSeeder::class,
+            OrderLogoSeeder::class,
+            SettingSeeder::class,
+            LogSeeder::class,
+        ], $databaseSeeder->seeders);
+    }
+}
